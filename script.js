709a0b701a0ee9bf307d956eb54249a8e09279b3
tsrapnik <tsrapnik>
2019-10-17 19:26:24 +0200

    added progress per week.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+Number.prototype.pad = function(size) {
+    let s = String(this);
+    while (s.length < (size || 2)) {s = "0" + s;}
+    return s;
+}
+
+//time_string should be of the format "hh:mm".
+function string_to_minutes(time_string) {
+    if ((time_string.length != 5) || (time_string[2] != ":"))
+        return NaN;
+    let hours =  parseInt(time_string.slice(0, 2));
+    let minutes = parseInt(time_string.slice(3, 5));
+    return hours * 60 + minutes;
+}
+
+//returns string in "hh:mm" format of the absolute value of the input.
+function minutes_to_string(minutes) {
+    minutes = Math.abs(minutes);
+    const max_minutes = 23 * 60 + 59;
+    if(!(minutes <= max_minutes))
+        //only return values from 00:00 to 23:59, all other cases (also nan) return an empty string.
+        return "";
+    let hours = Math.floor(minutes / 60);
+    minutes %= 60;
+    return hours.pad(2) + ":" + minutes.pad(2);
+}
+
 function task(update_parent) {
     let task = document.createElement("div");
     task.className = "task";
@@ -19,57 +46,42 @@ function task(update_parent) {
     stop_time.onchange = update_parent;
     task.appendChild(stop_time);
 
+    task.get_minutes = function () {
+        let minutes = string_to_minutes(stop_time.value) - string_to_minutes(start_time.value);
+        if(minutes >= 0)
+            return minutes;
+        else
+            return NaN;
+    }
+
     return task;
 }
 
-function time_progress() {
-    let time_progress = document.createElement("div");
-    time_progress.className = "time_progress";
-
-    let time_not_done = document.createElement("input");
-    time_not_done.className = "time_not_done";
-    time_not_done.type = "time";
-    time_not_done.readOnly = true;
-    time_not_done.value = "08:00"; //todo: provide const.
-
-    time_progress.appendChild(time_not_done);
-
-    return time_progress;
-}
-
-Number.prototype.pad = function(size) {
-    let s = String(this);
-    while (s.length < (size || 2)) {s = "0" + s;}
-    return s;
-}
-
-//time_string should be of the format "hh:mm".
-function string_to_minutes(time_string) {
-    if ((time_string.length != 5) || (time_string[2] != ":"))
-        return NaN;
-    let hours =  parseInt(time_string.slice(0, 2));
-    let minutes = parseInt(time_string.slice(3, 5));
-    return hours * 60 + minutes;
-}
-
-//returns string in "hh:mm" format of the absolute value of the input.
-function minutes_to_string(minutes) {
-    minutes = Math.abs(minutes);
-    let hours = Math.floor(minutes / 60);
-    minutes %= 60;
-    return hours.pad(2) + ":" + minutes.pad(2);
-}
-
-function day(dayName) {
+function day(dayName, update_parent) {
     let day = document.createElement("div");
     day.innerText = dayName;
     day.className = "day";
+    let minutes = 0;
+    day.get_minutes = () => minutes;
 
     let tasks = document.createElement("div");
     tasks.className = "tasks";
     day.appendChild(tasks);
 
-    day.appendChild(time_progress());
+    let required_minutes = document.createElement("input");
+    required_minutes.className = "required_minutes";
+    required_minutes.type = "time";
+    required_minutes.readOnly = true;
+    day.appendChild(required_minutes);
+    day.set_required_minutes = function (minutes) {
+        required_minutes.value = minutes_to_string(minutes);
+        if(minutes > 0) {
+            required_minutes.style.color = "rgba(255, 100, 100, 1.0)";
+        }
+        else if(minutes <= 0) {
+            required_minutes.style.color = "rgba(255, 255, 255, 0.55)";
+        }
+    }
 
     let add_task_button = document.createElement("button");
     add_task_button.className = "add_task_button";
@@ -80,25 +92,14 @@ function day(dayName) {
     day.appendChild(add_task_button);
 
     function update() {
-        let total_time = 0;
+        minutes = 0;
         for (let task of tasks.getElementsByClassName("task")) {
-            let start_time = string_to_minutes(task.getElementsByClassName("start_time")[0].value);
-            let stop_time = string_to_minutes(task.getElementsByClassName("stop_time")[0].value);
-            total_time += stop_time - start_time;
+            minutes += task.get_minutes();
         }
-        let time_progress = day.getElementsByClassName("time_progress")[0];
-        let time_not_done = time_progress.getElementsByClassName("time_not_done")[0];
-        time_not_done.value = minutes_to_string(8 * 60 - total_time); //todo: provide const.
 
-        console.log(total_time);
-        if(8 * 60 - total_time > 0) { //todo: provide const.
-            time_not_done.style.color = "rgba(255, 100, 100, 1.0)";
-        }
-        else if(8 * 60 - total_time <= 0) {
-            time_not_done.style.color = "rgba(255, 255, 255, 0.55)";
-        }
-        //todo: move logic for time to task?
+        update_parent();
     }
+
     return day;
 }
 
@@ -106,13 +107,23 @@ function week() {
     let week = document.createElement("div");
     week.className = "week";
 
-    let dayNames = ["monday", "tuesday", "wednesday", "thirsday", "friday"];
+    let dayNames = ["monday", "tuesday", "wednesday", "thursday", "friday"];
 
-    for (let index = 0; index < dayNames.length; index++) {
-        week.appendChild(day(dayNames[index]));
+    for (let dayName of dayNames) {
+        week.appendChild(day(dayName, update));
     }
 
     return week;
+
+    function  update() {
+        let required_minutes = 0; //gets accumulated for the whole week. if negative you worked more than needed.
+        const daily_required_minutes = 8 * 60;
+
+        for (let day of week.children) {
+            required_minutes += daily_required_minutes - day.get_minutes(); 
+            day.set_required_minutes(required_minutes);
+        }
+    }
 }
 
 let root = document.getElementById("root");
